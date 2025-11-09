6465a5fd38f6ebddb1103988c68c22b5ee8ad49d
YunJaeHoon <sleepzzz214@naver.com>
2025-11-09 16:19:34 +0900

    fix: index 페이지 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+import { BrowserRouter } from 'react-router-dom'
+
+// axios 기본 요청 URL 설정
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_SERVER_URI;
+
+// CORS 설정
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
