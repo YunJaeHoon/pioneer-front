6bb8d15b34381a9fc7aaaaa511a5091a52932b25
YunJaeHoon <sleepzzz214@naver.com>
2025-11-12 16:57:44 +0900

    feat: 헤더 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, createContext } from 'react'
 import axios from 'axios'
 import style from "./AppStyle.module.css"
 import { Route, Routes } from 'react-router-dom';
@@ -6,8 +6,14 @@ import HomePage from './page/HomePage';
 import Header from './component/Header';
 import Footer from './component/Footer';
 
+// Context API
+export const UserRoleContext = createContext();
+
 function App()
 {
+    // context
+    const [userRole, setUserRole] = useState(undefined);
+
     // 서버 연결 테스트
     useEffect(() => {
         async function testBackend() {
@@ -19,15 +25,17 @@ function App()
 
     return (
         <div id={style["container"]}>
+            <UserRoleContext.Provider value={{ userRole, setUserRole }}>
 
-            <Header />
-            
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
+                <Header />
+                
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
 
-            <Footer />
+                <Footer />
 
+            </UserRoleContext.Provider>
         </div>
     );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,19 +1,50 @@
 import { Link } from "react-router-dom";
 import style from "./HeaderStyle.module.css"
 
-import logo from "../asset/logo.svg"
+import logo from "../asset/logo.svg";
+import loginIcon from "../asset/login-icon.svg";
+import linkLeftParentheses from "../asset/link-left-parentheses.svg";
+import linkRightParentheses from "../asset/link-right-parentheses.svg";
+import { UserRoleContext } from "../App";
+import { useContext } from "react";
+
+function HeaderLink({ to, label })
+{
+    return (
+        <Link to={to} className={style["link-container"]}>
+            <img src={linkLeftParentheses} className={style["link-decorator"]}/>
+            <div className={style["link-text"]}>{label}</div>
+            <img src={linkRightParentheses} className={style["link-decorator"]}/>
+        </Link>
+    );
+}
 
 function Header()
 {
+    // context
+    const { userRole } = useContext(UserRoleContext);
+
     return (
         <div id={style["main-container"]}>
             <div id={style["left-container"]}>
-                <Link to="/">
-                    <img src={logo} id={style["logo"]}/>
+                <Link to="/" id={style["logo"]}>
+                    <img src={logo} className={style["btn"]}/>
                 </Link>
+                <HeaderLink to="/fight" label="결투" />
+                <HeaderLink to="/weapons" label="무기고" />
+                <HeaderLink to="/challenges" label="도전 과제" />
+                <HeaderLink to="/records" label="기록장" />
             </div>
             <div id={style["right-container"]}>
-                오른쪽
+                {
+                    userRole === undefined ? (
+                        <Link to="/login">
+                            <img src={loginIcon} id={style["login-icon"]} className={style["btn"]}/>
+                        </Link>
+                    ) : (
+                        <div>완료</div>
+                    )
+                }
             </div>
         </div>
     );
